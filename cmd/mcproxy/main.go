ac8b7a029f64f6e6847c82b493483c93a5f137cd
ken343 <kmcdole21@gmail.com>
2020-02-01 18:10:32 -0600

    Forgot to Save main.go

--- cmd/mcproxy/main.go
+++ cmd/mcproxy/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+)
+
+// Need to figure out what constants I need.
+const (
+	RPORT = ":8080" // RPORT for Reverse Proxy port to listen on. Replace with proxy type.
 )
 
 func main() {
 	fmt.Println("Howdy?")
+
+	http.ListenAndServe(RPORT, nil)
 }
